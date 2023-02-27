5919f543a7af3ecc2beec68fc23732fbce95558d
JunH <tjdtls690@gmail.com>
2023-02-27 16:18:31 +0900

    :white_check_mark: test : Bar 가 존재하는지 여부를 알려주는 메서드의 단위 테스트 추가

--- src/test/java/ladder/domain/BarTest.java
+++ src/test/java/ladder/domain/BarTest.java
@@ -26,4 +26,18 @@ class BarTest {
         // then
         assertThat(bar).isEqualTo(Bar.FALSE);
     }
+    
+    @Test
+    @DisplayName("Bar.TRUE의 isExistBar 메서드를 호출하면 true가 나온다.")
+    void isExistBar_true() {
+        // given & when& & then
+        assertThat(Bar.TRUE.isExistBar()).isTrue();
+    }
+    
+    @Test
+    @DisplayName("Bar.FALSE의 isExistBar 메서드를 호출하면 false가 나온다.")
+    void isExistBar_false() {
+        // given & when& & then
+        assertThat(Bar.FALSE.isExistBar()).isFalse();
+    }
 }
